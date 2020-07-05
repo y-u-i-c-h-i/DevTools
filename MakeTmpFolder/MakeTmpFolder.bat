3d925fdf285df46cb9b1030444c45143d1a4e485
shiomi <shiomi376@gmail.com>
2020-07-05 16:35:18 +0900

    事前に古いフォルダを削除するようにした

--- MakeTmpFolder/MakeTmpFolder.bat
+++ MakeTmpFolder/MakeTmpFolder.bat
@@ -1,2 +1,10 @@
 
+
+cd /d C:\local\work\tmpfolder
+
+rem dir /b /o-n：カレント以下のファイル名を降順で一覧にする
+rem skip：一覧の中で先頭から30行をスキップする
+rem rd /s /q：サブディレクトリごと削除、quietなので削除時の確認をなしにする
+for /f "skip=30" %%A in ('dir /b /o-n') do rd /s /q "%%A"
+
 %~dp0\MakeTmpFolder.vbs
